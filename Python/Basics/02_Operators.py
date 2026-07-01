294a6298ec1fb9e0b95693f54185838963e1db2d
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-07-01 11:26:26 +0530

    Complete Mission 2: Python Operators

---
+++ Python/Basics/02_Operators.py
@@ -0,0 +1,66 @@
+# =============================
+# Arithmetic Operators
+# =============================
+
+a = 5
+b = 10
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+print("Floor Division:", a // b)
+
+# =============================
+# Assignment Operators
+# =============================
+
+score = 20
+
+print("Score", score)  
+
+score += 20
+print(score)
+
+score -= 5
+print(score)
+
+money = 50
+
+print("Money", money)
+
+money *= 2
+print(money)
+
+money /= 4
+print(money)
+
+# =============================
+# Comparison Operators
+# =============================
+
+age = 22
+
+print(age > 18)
+print(age < 18)
+print(age == 20)
+print(age != 22)
+print(age >= 22)
+print(age <= 21)
+
+# =============================
+# Logical Operators
+# =============================
+
+is_student = True
+has_id = True
+
+print (is_student and has_id)  # True
+
+print (is_student or has_id)   # True
+
+print (not is_student)          # False
+
+
